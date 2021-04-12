9eb44e32e2299c313b80a934eb041c70684de666
John Wan Ng <johnwanng@Johns-MBP.lan>
2021-04-12 22:00:06 +0800

    commit

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,12 +1,12 @@
 // Define SVG area dimensions
 var svgWidth = 960;
-var svgHeight = 660;
+var svgHeight = 500;
 
 // Define the chart's margins as an object
 var chartMargin = {
-    top: 100,
-    right: 30,
-    bottom: 100,
+    top: 20,
+    right: 40,
+    bottom: 60,
     left: 100
   };
 
@@ -63,151 +63,57 @@ d3.csv("assets/data/data.csv").then(function(Data) {
     chartGroup.append("g")
               .call(d3.axisLeft(y));
 
-
-/*
-
-// Step 1: Append tooltip div
-    var toolTip = d3.select("body").append("div").classed("tooltip",true);
-
-    // Step 2: Create "mouseover" event listener to display tooltip
-    circlesGroup.on("mouseover", function(d, i) {
-      toolTip.style("display", "block");
-      toolTip.html('<p>'  + d.date + '<br>' + d.medals + ' medal(s) <br> won </p>')
-        .style("left", d3.event.pageX + "px")
-        .style("top", d3.event.pageY + "px");
-    })
-      // Step 3: Add an onmouseout event to make the tooltip invisible
-      .on("mouseout", function() {
-        toolTip.style("display", "none");
-      });
-
-*/
-
-
-    // Add the tooltip container to the body
-    /* var toolTip = d3.select("body")
-                    .append("div")
-                    .classed("tooltip",true); */
-
-    var toolTip = d3.tip()
-                    .attr("class", "tooltip")
+   // Define tooltip
+   var toolTip = d3.tip()
+                    .attr("class", "d3-tip")
                     .offset([80, -60])
                     .html(function(d) {
+                      console.log(`${d.state}<br> Poverty : ${d.poverty} <br> HealthCars: ${d.healthcare}`);
                       return (`${d.state}<br> Poverty : ${d.poverty} <br> HealthCars: ${d.healthcare}`);
                     });                   
-/*
 
+    // Add Circles
+    var circlesGroup = chartGroup.selectAll("Circle")
+                                 .data(Data)
+                                 .enter()
+                                 .append("circle")
+                                 .attr("cx", function (d) { return x(d.poverty); } )
+                                 .attr("cy", function (d) { return y(d.healthcare); } )
+                                 .attr("r", 10)        
+                                 .style("fill", "#8abcd5");
 
-var toolTip = d3.tip()
-    .attr("class", "tooltip")
-    .offset([80, -60])
-    .html(function(d) {
-      return (`${d.rockband}<br>${label} ${d[chosenXAxis]}`);
-    });
-
-  circlesGroup.call(toolTip);
-
-  circlesGroup.on("mouseover", function(data) {
-    toolTip.show(data);
-  })
-    // onmouseout event
-    .on("mouseout", function(data, index) {
-      toolTip.hide(data);
-    });
-
-
-*/
-
-
-    // tooltip mouseover event handler
- /*    var tipMouseover = function(d) {
-        var html  = '<p>' + d.state + "<br/>" +
-                    "Poverty : " + d.poverty + "<br/>" +
-                    "HealthCare : " + d.healthcare + '</p>';
-        console.log(html);
-        //console.log(d3.event.pageX);
-        //console.log(d3.event.pageY);
-        tooltip.html(html)
-                .style("left", (d3.event.pageX + 15) + "px")
-                .style("top", (d3.event.pageY - 28) + "px")
-                .transition()
-                .duration(200) // ms
-                .style("opacity", .9) // started as 0!
-    };
-
-    // tooltip mouseout event handler
-    var tipMouseout = function(d) {
-        tooltip.transition()
-               .duration(300) // ms
-               .style("opacity", 0); // don't care about position!
-    }; */
-
-    // Add dots and circles
-    var gdots = chartGroup.selectAll("dot")
-                          .data(Data)
-                          .enter()
-                          .append("g");
-
-    gdots.append("circle")
-         .attr("class","dot")
-         .attr("cx", function (d) { return x(d.poverty); } )
-         .attr("cy", function (d) { return y(d.healthcare); } )
-         .attr("r", 10)        
-         .style("fill", "#8abcd5");
-
-    //.on("mouseover", tipMouseover)
-
-    //.on("mouseout", tipMouseout);         
+    var circleLabels = chartGroup.selectAll(null).data(Data).enter().append("text");
 
     // text label for the circle
-    gdots.append("text").text(function(d) {return d.abbr})
-         .attr("x", function(d) {return x(d.poverty);})
-         .attr("y", function(d) {return y(d.healthcare);})
-         .attr("text-anchor","middle")
-         .style("font-size","9px")
-         .style("fill", "white");
+    circleLabels.attr("x", function(d) {return x(d.poverty);})
+                .attr("y", function(d) {return y(d.healthcare);})
+                .text(function(d) {return d.abbr})
+                .attr("text-anchor","middle")
+                .style("font-size","9px")
+                .style("fill", "white");
 
 
-    chartGroup.call(toolTip);
+    circleLabels.call(toolTip);
 
-    chartGroup.on("mouseover", function(data) {
-      toolTip.show(data);
-    })
-      // onmouseout event
-      .on("mouseout", function(data, index) {
-        toolTip.hide(data);
-      });
+                  // onmouseover event
+    circleLabels.on("mouseover", function(data) {
+                    toolTip.show(data);})
+                  // onmouseout event
+                .on("mouseout", function(data, index) {
+                    toolTip.hide(data);});
 
-         /* 
-         // Add mouseoiver event to display tooltip
-         .on("mouseover", function(d, i) {
-            console.log('mouseover');
-            toolTip.style("display", "block");
-            toolTip.html('<p>' + d.state + "<br/>" + "Poverty : " + d.poverty + "<br/>" + "HealthCare : " + d.healthcare + '</p>')
-                  .style("left", d3.event.pageX + "px")
-                  .style("top", d3.event.pageY + "px");
-              })
-          // Add an onmouseout event to make the tooltip invisible
-         .on("mouseout", function() {
-            console.log('mouseout');
-            toolTip.style("display", "none");
-          });
- */
     // text label for the x axis
     chartGroup.append("text")
-              .attr("transform", "translate(" + ((svgWidth / 2) - 30 ) + " ," + (svgHeight - chartMargin.bottom - 50) + ")")  
-              .style("text-anchor", "middle")
-              .style("font-weight","bold")
+              .attr("transform", "translate(" + ((svgWidth / 2 - 80)) + " ," + (svgHeight - chartMargin.top - 10) + ")")                
               .text("In Poverty (%)");
 
     // text label for the y axis
     chartGroup.append("text")
               .attr("transform", "rotate(-90)")
-              .attr("y", 0 - chartMargin.left + 50)
-              .attr("x", 0 - (svgHeight / 2) + 100)
+              .attr("y", 0 - chartMargin.left + 40)
+              .attr("x", 0 - (svgHeight / 2))
               .attr("dy", "1em")
-              .style("text-anchor", "middle")
-              .style("font-weight","bold")
+              //.style("font-weight","bold")
               .text("Lacks Healthcare (%)"); 
 
 });
